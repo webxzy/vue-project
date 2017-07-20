864a1cfe6c74add2ef28a8d8814b820dffff3043
webxzy <webxzy@qq.com>
2017-07-21 01:39:39 +0800

    todo-list

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>首页</h1>
+    <Todo />
+  </div>
+</template>
+
+<script>
+
+import Todo from './Todo';
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Todo
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-list">
+    <h2>todo-list</h2>
+    <form action="#" v-on:submit="submit">
+      <input v-model="input" type="text" />
+      <button>添加</button>
+    </form>
+    <ul class="list">
+      <li v-for="(item, index) in items" :key="item.id">
+        {{item.text}}
+        <span v-on:click="items.splice(index, 1)">&times;</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data() {
+    return {
+      input: '',
+      items: []
+    }
+  },
+  methods: {
+    submit: function (e) {
+      if (this.input.trim() === '') return;
+      this.items.push({ id: this.items.length + 1, text: this.input })
+      this.input = '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list span {
+  cursor: pointer;
+  color: red;
+}
+</style>
